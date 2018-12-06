bdf86ed2f3f52a1b405c624c57762affe8188069
Rebecca Strauss <rstrauss127@gmail.com>
2018-12-06 21:22:34 +0000

    RubyWeeklySCraper working

--- lib/rubyweekly_scraper.rb
+++ lib/rubyweekly_scraper.rb
@@ -8,4 +8,29 @@ class RubyWeeklyScraper
     @doc = Nokogiri::HTML(open("https://rubyweekly.com/issues/#{issue_number}"))
   end
 
+  def scrape
+    scrape_details
+    @newsletter #=> This instance should have a bunch of articles and details
+  end
+
+  def scrape_details
+    # populate @newsletter with more data from the newsletter site
+    @newsletter.issue_date = @doc.search("table.gowide.lonmo").text.gsub("Issue #{@issue_number} - ", "").strip
+  end
+
+  def scrape_articles
+    #break convention of knly knowing about the Newsletter and let it create articles
+    @doc.search("td[align = 'left'] table.gowide")[2..-1].each do |article_table|
+      #instantiate the article
+      a = Article.new
+      #scrape the data
+      a.author = article_table.search("div:first").text.strip
+      a.title = article_table.search("a:first").text.strip
+      a.url = article_table.search("a:first").attr("href").text.strip
+      #add the article to the newsletter
+      @newsletter.add_articles(a)
+
+    end
+  end
+
 end
